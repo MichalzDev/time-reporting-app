76cd4f60309cb1a8d0420968e78490e3508843b5
Czaroos <czaroosik@gmail.com>
2020-06-08 12:37:59 +0200

    sign in works

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,76 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import Container from 'react-bootstrap/Container'
-import Navbar from 'react-bootstrap/Navbar'
-import NavDropdown from 'react-bootstrap/NavDropdown'
+import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import HomeScreen from './components/home-screen.component';
+import SignIn from "./components/signin";
+import CreateUser from "./components/create-user.component";
+import ReadUser from "./components/read-user.component";
+import EditUser from "./components/edit-user.component";
 
-import CreateUser from './components/create-user.component';
-import ReadUser from './components/read-user.component';
-import EditUser from './components/edit-user.component';
-
-import CreateProject from './components/create-project.component';
-import ReadProject from './components/read-project.component';
-import EditProject from './components/edit-project.component';
-import CreateReport from './components/create-report.component';
-import ReadReport from './components/read-report.component';
-import EditReport from './components/edit-report.component';
-
+import CreateProject from "./components/create-project.component";
+import ReadProject from "./components/read-project.component";
+import EditProject from "./components/edit-project.component";
+import CreateReport from "./components/create-report.component";
+import ReadReport from "./components/read-report.component";
+import EditReport from "./components/edit-report.component";
 
 class App extends Component {
+  state = {
+    login: "",
+    password: "",
+    permission: "",
+    redirect: null,
+  };
+
+  onChangeLogin = (e) => {
+    this.setState({
+      login: e.target.value,
+    });
+  };
+
+  onChangePassword = (e) => {
+    this.setState({
+      password: e.target.value,
+    });
+  };
+
+  onSubmit = () => {
+    fetch(
+      "http://localhost:5000/users/" +
+        this.state.login +
+        "/" +
+        this.state.password
+    )
+      .then((res) => res.json())
+      .then((res) =>
+        this.setState({
+          permission: res[0].user_permissions,
+          redirect: '/' + res[0].user_permissions
+        })
+      );
+  };
+
   render() {
+    if (this.state.redirect) {
+      return (
+        <Router>
+          <Route path="/user" exact component={ReadUser} /> {/* do zmiany */}
+          <Redirect to={this.state.redirect} />
+        </Router>
+      );
+    }
     return (
       <Router>
         <Container>
-          <Navbar bg="light" expand="lg">
+          {/* <Navbar bg="light" expand="lg">
             <Navbar.Brand href="/" >Control panel</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
@@ -41,17 +87,26 @@ class App extends Component {
                 <NavDropdown.Item><Link to='/reports/create'>Create Report</Link></NavDropdown.Item>
               </NavDropdown>
             </Navbar.Collapse>
-          </Navbar>
-          <Route path='/' exact component = {HomeScreen} />
-          <Route path='/users' exact component = {ReadUser} />
-          <Route path='/users/create' exact component = {CreateUser} />
-          <Route path='/users/edit/:id' exact component = {EditUser} />
-          <Route path='/projects' exact component = {ReadProject} />
-          <Route path='/projects/create' exact component = {CreateProject} />
-          <Route path='/projects/edit/:id' exact component = {EditProject} />
-          <Route path='/reports' exact component = {ReadReport} />
-          <Route path='/reports/create' exact component = {CreateReport} />
-          <Route path='/reports/edit/:id' exact component = {EditReport} />
+          </Navbar> */}
+          <Route
+            path="/"
+            render={(props) => (
+              <SignIn
+                {...props}
+                onChangeLogin={this.onChangeLogin}
+                onChangePassword={this.onChangePassword}
+                onSubmit={this.onSubmit}
+              />
+            )}
+          />
+          <Route path="/users/create" exact component={CreateUser} />
+          <Route path="/users/edit/:id" exact component={EditUser} />
+          <Route path="/projects" exact component={ReadProject} />
+          <Route path="/projects/create" exact component={CreateProject} />
+          <Route path="/projects/edit/:id" exact component={EditProject} />
+          <Route path="/reports" exact component={ReadReport} />
+          <Route path="/reports/create" exact component={CreateReport} />
+          <Route path="/reports/edit/:id" exact component={EditReport} />
         </Container>
       </Router>
     );
