b654dc4591a9ec4ae4de4daa0e9fba1859ad4205
MichalzDev <51170703+MichalzDev@users.noreply.github.com>
2020-06-09 09:53:59 +0200

    adds pdf

--- client/src/components/sortableTable.js
+++ client/src/components/sortableTable.js
@@ -1,15 +1,15 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
-import PictureAsPdfIcon from '@material-ui/icons/PictureAsPdf';
+import PictureAsPdfIcon from "@material-ui/icons/PictureAsPdf";
 import "bootstrap/dist/css/bootstrap.min.css";
-import PDF from './pdf';
+import PDF from "./pdf";
 
 const FilterableTable = require("react-filterable-table");
 
-const Table = ({reports, redirect}) => {
+const Table = ({ reports, redirect }) => {
   const fields = [
     {
       name: "report_project",
@@ -48,50 +48,71 @@ const Table = ({reports, redirect}) => {
   ];
 
   const acceptAll = () => {
-    fetch('http://localhost:5000/reports/acceptAll', {
-      method: 'PUT'
-    })
+    fetch("http://localhost:5000/reports/acceptAll", {
+      method: "PUT",
+    });
 
-    redirect('/supervisor')
-  }
+    redirect("/supervisor");
+  };
 
   const acceptOne = (id) => {
-    fetch('http://localhost:5000/reports/acceptOne/' + id, {
-      method: 'PUT'
-    })
+    fetch("http://localhost:5000/reports/acceptOne/" + id, {
+      method: "PUT",
+    });
 
-    redirect('/supervisor')
-  }
+    redirect("/supervisor");
+  };
 
   const rejectOne = (id) => {
-    fetch('http://localhost:5000/reports/rejectOne/' + id, {
-      method: 'PUT'
-    })
+    fetch("http://localhost:5000/reports/rejectOne/" + id, {
+      method: "PUT",
+    });
 
-    redirect('/supervisor')
-  }
+    redirect("/supervisor");
+  };
 
   return (
     <Container>
-      <Button type="submit" variant="warning" className="mb-3" onClick={() => acceptAll()}>AKCEPTUJ WSZYSTKIE NIEZWERYFIKOWANE</Button>
-        <Row>
-            <Col xs={9}>
-      <FilterableTable
-        data={reports}
-        fields={fields}
-      />
-      </Col>
-      <Col xs={3} style={{paddingTop: '130px'}}>
-      {reports.map((row, index) => {
-          return <Row className="mt-2">
-              <Button type="submit" variant="success" size="sm" className="ml-2 mt-2" onClick={() => acceptOne(row._id)}>AKCEPTUJ</Button>
-              <Button type="submit" variant="danger" size="sm" className="ml-2 mt-2" onClick={() => rejectOne(row._id)}>ODRZUC</Button>
-          </Row>
-        
-      })}
-      </Col>
+      <Button
+        type="submit"
+        variant="warning"
+        className="mb-3"
+        onClick={() => acceptAll()}
+      >
+        AKCEPTUJ WSZYSTKIE NIEZWERYFIKOWANE
+      </Button>
+      <Row>
+        <Col xs={9}>
+          <FilterableTable data={reports} fields={fields} />
+        </Col>
+        <Col xs={3} style={{ paddingTop: "130px" }}>
+          {reports.map((row, index) => {
+            return (
+              <Row className="mt-2">
+                <Button
+                  type="submit"
+                  variant="success"
+                  size="sm"
+                  className="ml-2 mt-2"
+                  onClick={() => acceptOne(row._id)}
+                >
+                  AKCEPTUJ
+                </Button>
+                <Button
+                  type="submit"
+                  variant="danger"
+                  size="sm"
+                  className="ml-2 mt-2"
+                  onClick={() => rejectOne(row._id)}
+                >
+                  ODRZUC
+                </Button>
+              </Row>
+            );
+          })}
+        </Col>
       </Row>
-      <PDF reports={reports}/>
+      <PDF reports={reports} />
     </Container>
   );
 };
--- client/src/components/supervisor.js
+++ client/src/components/supervisor.js
@@ -23,7 +23,7 @@ import CreateReport from "./create-report.component";
 import ReadReport from "./read-report.component";
 import EditReport from "./edit-report.component";
 import Raport from "./raport";
-import SortableTable from './sortableTable';
+import SortableTable from "./sortableTable";
 
 class User extends Component {
   state = {
@@ -58,7 +58,7 @@ class User extends Component {
               <Button
                 variant="secondary"
                 className="m-2"
-                onClick={() => this.props.redirect('/projects')}
+                onClick={() => this.props.redirect("/projects")}
               >
                 Wszystkie projekty
               </Button>
@@ -75,17 +75,22 @@ class User extends Component {
               </Button>
             </Navbar.Collapse>
           </Navbar>
-          {/* <Route path="/users/" exact component={ReadUser} />
-          <Route path="/users/create" exact component={CreateUser} />
-          <Route path="/users/edit/:id" exact component={EditUser} />
+          {/* <Route path="/users/" exact component={ReadUser} /> */}
+          {/* <Route path="/users/create" exact component={CreateUser} /> */}
+          {/* <Route path="/users/edit/:id" exact component={EditUser} /> */}
           <Route path="/projects" exact component={ReadProject} />
           <Route path="/projects/create" exact component={CreateProject} />
           <Route path="/projects/edit/:id" exact component={EditProject} />
-          <Route path="/reports" exact component={ReadReport} />
-          <Route path="/reports/create" exact component={CreateReport} />
-          <Route path="/reports/edit/:id" exact component={EditReport} /> */}
-          <h3 style={{textAlign: 'center'}} className="mt-3 mb-3">Raporty</h3>
-          <SortableTable reports={this.state.reports} redirect={this.props.redirect}/>
+          {/* <Route path="/reports" exact component={ReadReport} /> */}
+          {/* <Route path="/reports/create" exact component={CreateReport} /> */}
+          {/* <Route path="/reports/edit/:id" exact component={EditReport} /> */}
+          <h3 style={{ textAlign: "center" }} className="mt-3 mb-3">
+            Raporty
+          </h3>
+          <SortableTable
+            reports={this.state.reports}
+            redirect={this.props.redirect}
+          />
         </Container>
       </Router>
     );
