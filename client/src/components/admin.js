9a64e296a70f473b0ff20ee46b754d0193171d8c
Czaroos <czaroosik@gmail.com>
2020-06-09 02:10:27 +0200

    bug fixes

--- client/src/components/admin.js
+++ client/src/components/admin.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -9,7 +9,7 @@ import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Button from 'react-bootstrap/Button'
+import Button from "react-bootstrap/Button";
 
 import CreateUser from "./create-user.component";
 import ReadUser from "./read-user.component";
@@ -21,60 +21,91 @@ import EditProject from "./edit-project.component";
 import CreateReport from "./create-report.component";
 import ReadReport from "./read-report.component";
 import EditReport from "./edit-report.component";
+import Raport from './raport';
 
-export default function Admin(props) {
-  return (
-    <Router>
-      <Container>
-        <Navbar bg="light" expand="lg">
-          <Navbar.Brand>Control panel</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <NavDropdown title="Users" id="basic-nav-dropdown">
-              <NavDropdown.Item>
-                <Link to="/users">Users List</Link>
-              </NavDropdown.Item>
-              <NavDropdown.Item>
-                <Link to="/users/create">Create User</Link>
-              </NavDropdown.Item>
-            </NavDropdown>
-            <NavDropdown title="Projects" id="basic-nav-dropdown">
-              <NavDropdown.Item>
-                <Link to="/projects">Projects List</Link>
-              </NavDropdown.Item>
-              <NavDropdown.Item>
-                <Link to="/projects/create">Create Project</Link>
-              </NavDropdown.Item>
-            </NavDropdown>
-            <NavDropdown title="Reports" id="basic-nav-dropdown">
-              <NavDropdown.Item>
-                <Link to="/reports">Reports List</Link>
-              </NavDropdown.Item>
-              <NavDropdown.Item>
-                <Link to="/reports/create">Create Report</Link>
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Button
-              type="button"
-              onClick={() => {
-                fetch("http://localhost:5000/users/logout", {
-                  method: "PUT",
-                });
-                props.redirect(null);
-              }}
-            >WYLOGUJ</Button>
-          </Navbar.Collapse>
-        </Navbar>
-        <Route path="/users/" exact component={ReadUser} />
-        <Route path="/users/create" exact component={CreateUser} />
-        <Route path="/users/edit/:id" exact component={EditUser} />
-        <Route path="/projects" exact component={ReadProject} />
-        <Route path="/projects/create" exact component={CreateProject} />
-        <Route path="/projects/edit/:id" exact component={EditProject} />
-        <Route path="/reports" exact component={ReadReport} />
-        <Route path="/reports/create" exact component={CreateReport} />
-        <Route path="/reports/edit/:id" exact component={EditReport} />
-      </Container>
-    </Router>
-  );
+class Admin extends Component {
+  state = {
+    reports: [],
+  };
+
+  fetchReports = () => {
+    fetch("http://localhost:5000/reports/")
+      .then((res) => res.json())
+      .then((res) =>
+        this.setState({
+          reports: res,
+        })
+      );
+  };
+
+  componentDidMount() {
+    this.fetchReports();
+  }
+
+  render() {
+    console.log(this.state)
+    return (
+      <Router>
+        <Container>
+          <Navbar bg="light" expand="lg">
+            <Navbar.Brand>Control panel</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <NavDropdown title="Users" id="basic-nav-dropdown">
+                <NavDropdown.Item>
+                  <Link to="/users">Users List</Link>
+                </NavDropdown.Item>
+                <NavDropdown.Item>
+                  <Link to="/users/create">Create User</Link>
+                </NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown title="Projects" id="basic-nav-dropdown">
+                <NavDropdown.Item>
+                  <Link to="/projects">Projects List</Link>
+                </NavDropdown.Item>
+                <NavDropdown.Item>
+                  <Link to="/projects/create">Create Project</Link>
+                </NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown title="Reports" id="basic-nav-dropdown">
+                <NavDropdown.Item>
+                  <Link to="/reports">Reports List</Link>
+                </NavDropdown.Item>
+                <NavDropdown.Item>
+                  <Link to="/report/create">Create Report</Link>
+                </NavDropdown.Item>
+              </NavDropdown>
+              <Button
+                type="button"
+                onClick={() => {
+                  fetch("http://localhost:5000/users/logout", {
+                    method: "PUT",
+                  });
+                  this.props.redirect(null);
+                }}
+              >
+                WYLOGUJ
+              </Button>
+            </Navbar.Collapse>
+          </Navbar>
+          <Route path="/users/" exact component={ReadUser} />
+          <Route path="/users/create" exact component={CreateUser} />
+          <Route path="/users/edit/:id" exact component={EditUser} />
+          <Route path="/projects" exact component={ReadProject} />
+          <Route path="/projects/create" exact component={CreateProject} />
+          <Route path="/projects/edit/:id" exact component={EditProject} />
+          <Route
+            path="/reports"
+            render={(props) => (
+              <Raport {...props} redirect={this.props.redirect} reports={this.state.reports}/>
+            )}
+          />
+          <Route path="/report/create" exact component={CreateReport} />
+          <Route path="/reports/edit/:id" exact component={EditReport} />
+        </Container>
+      </Router>
+    );
+  }
 }
+
+export default Admin;
